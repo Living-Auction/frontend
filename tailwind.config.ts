be685715ea42c81968dfbdcb932891088594e7cc
젤리 <1gyn093@gmail.com>
2025-09-24 18:45:52 +0900

    chore: config 그림자 스타일 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,6 +43,11 @@ const config = {
         medium: '600',
         regular: '400',
       },
+      shadow: {
+        lg: '0 12px 24px 0 rgba(0, 0, 0, 0.2)',
+        basic: '0 4px 16px 0 rgba(0, 0, 0, 0.08)',
+        sm: '0 2px 4px 0 rgba(0, 0, 0, 0.04)',
+      },
     },
   },
   plugins: [],
