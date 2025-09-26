e63bb4c6bfeafc83d833881c0470520d66477982
전민정 <minj_1203@naver.com>
2025-09-27 02:35:47 +0900

    refactor: 헤더 위치 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import MainLayout from '@/widgets/layout/ui/main';
+import MainHeader from '@/widgets/header/ui/main';
 
 export default function Home() {
   return (
     <MainLayout>
+      <MainHeader />
       <section>
         <h2 className='text-title'>메인페이지</h2>
       </section>
--- src/widgets/layout/ui/main.tsx
+++ src/widgets/layout/ui/main.tsx
@@ -1,4 +1,3 @@
-import MainHeader from '@/widgets/header/ui/main';
 import Navigation from '@/widgets/footer/ui/navigation';
 
 interface MainLayoutProps {
@@ -8,7 +7,6 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className='relative bg-background md:max-w-md lg:max-w-xl mx-auto min-h-screen w-full flex flex-col'>
-      <MainHeader />
       <main className='flex-1'>{children}</main>
       <Navigation />
     </div>
