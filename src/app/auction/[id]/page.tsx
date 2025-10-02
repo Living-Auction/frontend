2d7a6a9d4ced5e282ef6e49ae7145fcfd5d319c7
젤리 <1gyn093@gmail.com>
2025-10-03 00:24:22 +0900

    Revert "feat: carousel 생성"
    This reverts commit 9d9ef0c8e8691f8b95efe4ba4bf709f66259ca76.

--- src/app/auction/[id]/page.tsx
+++ src/app/auction/[id]/page.tsx
@@ -1,17 +1,9 @@
 import AuctionDetailHeader from '@widgets/header/ui/auction-detail';
-import Carousel from '@/widgets/carousel/ui/carousel';
 
 const AuctionDetail = () => {
   return (
     <>
       <AuctionDetailHeader />
-      <Carousel auctionId={'test-id-121231234'} />
-      <article>
-        <section>overview</section>
-        <section>profile card</section>
-        <section>detail contents: text & bid-status-table</section>
-        <footer>bid-banner</footer>
-      </article>
     </>
   );
 };
--- src/entities/auction/model/types.ts
+++
@@ -1,4 +0,0 @@
-export interface AuctionImage {
-  id: number;
-  url: string;
-}
