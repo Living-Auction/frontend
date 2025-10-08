ce27e31f3fc9a9749009247cb37bdfc3cff5335b
젤리 <1gyn093@gmail.com>
2025-10-08 19:14:52 +0900

    feat: overview 컴포넌트 생성 및 페이지 퍼블리싱

---
+++ src/widgets/product/product-overview.tsx
@@ -0,0 +1,40 @@
+import ProductEngagement from '@/entities/product/ui/product-engagement';
+import ProductStatusBadge from '@/entities/product/ui/product-status-badge';
+import { ProductTimer } from '@/features/product-timer';
+import { AuctionStatus } from '@/features/product-timer/model/types';
+import ShareIconButton from '@/features/share-product/share-icon-button';
+
+interface ProductOverviewProps {
+  status: AuctionStatus;
+  title: string;
+  views: number;
+  favorites: number;
+  startDate: string;
+  endDate: string;
+}
+
+const ProductOverview = ({
+  status,
+  title,
+  views,
+  favorites,
+  startDate,
+  endDate,
+}: ProductOverviewProps) => {
+  return (
+    <section className='bg-background p-4'>
+      <ProductStatusBadge status={status} />
+      <h3 className='text-subtitle font-bold text-ellipsis line-clamp-2 my-3'>{title}</h3>
+      <div className='flex items-center w-full justify-between mb-6'>
+        <div className='flex item-center gap-4'>
+          <ProductEngagement type={'views'} counts={views} />
+          <ProductEngagement type={'favorites'} counts={favorites} />
+        </div>
+        <ShareIconButton />
+      </div>
+      <ProductTimer status={status} startDate={startDate} endDate={endDate} />
+    </section>
+  );
+};
+
+export default ProductOverview;
