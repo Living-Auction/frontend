00f07c684ea86e172c4b40067c4616cdfa3ceb9b
젤리 <1gyn093@gmail.com>
2025-10-12 00:30:17 +0900

    chore: DUMMY constatns 및 type 정의

---
+++ src/entities/user/model/constants.ts
@@ -0,0 +1,18 @@
+export const PROFILECARD_DUMMY: Profilecard = {
+  uuid: 'user-1234',
+  nickName: '아띠',
+  img: 'https://picsum.photos/id/582/300/400',
+  is_online: true,
+};
+
+export const PROFILECARD_DUMMY2: Profilecard = {
+  uuid: 'user-5678',
+  nickName: '코야',
+  last_seen_at: '2025-10-11T08:33:04Z',
+};
+
+export const PROFILECARD_DUMMY3: Profilecard = {
+  uuid: 'user-90190',
+  nickName: '기니',
+  last_seen_at: '2025-10-11T02:33:04Z',
+};
