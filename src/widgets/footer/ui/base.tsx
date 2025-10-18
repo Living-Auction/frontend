dd2d432b679cd449d161f5e9bea012fe3baf7f86
젤리 <1gyn093@gmail.com>
2025-10-18 19:39:18 +0900

    style: footer 영역 높이값 auto 수정

--- src/widgets/footer/ui/base.tsx
+++ src/widgets/footer/ui/base.tsx
@@ -1,7 +1,7 @@
 const BaseFooter = ({ children, className }: { children: React.ReactNode; className?: string }) => {
   return (
     <footer
-      className={`w-full h-22 fixed bottom-0 left-0 right-0 md:max-w-md rounded-t-2xl pt-2 pb-6 flex px-4 justify-between bg-background shadow-lg items-center z-20 ${className}`}
+      className={`w-full min-h-22 h-auto fixed bottom-0 left-0 right-0 md:max-w-md rounded-t-2xl pt-2 pb-6 flex px-4 justify-between bg-background shadow-lg items-center z-20 ${className}`}
     >
       {children}
     </footer>
